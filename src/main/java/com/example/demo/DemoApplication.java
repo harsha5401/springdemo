8213647d70db4aeed5c7c85c5c74f345949a3d1b
harsha5401 <98007205+harsha5401@users.noreply.github.com>
2023-08-07 22:49:32 +0530

    Update DemoApplication.java

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -8,9 +8,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class DemoApplication {
 	
 	// @GetMapping
-	// public String message(){
-	// 	return "welcome to devops pipeline";
-	// }
+	 public String message(){
+	 	return "welcome to devops pipeline";
+	 }
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
