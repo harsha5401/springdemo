38e3664a80c97366e4279a04df478d7d924505f3
harsha5401 <98007205+harsha5401@users.noreply.github.com>
2023-08-07 23:02:04 +0530

    Update DemoApplication.java

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,16 +1,22 @@
 package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 // import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class DemoApplication {
 	
-	// @GetMapping
-	 public String message(){
-	 	return "welcome to devops pipeline";
-	 }
+	@RestController
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String helloWorld() {
+        return "Hello, World!";
+    }
+}
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
